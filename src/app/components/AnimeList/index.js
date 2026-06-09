f7a754fbc34e83757107767bee473f8959b2eb66
Roma Debrian <roma_coll04@yahoo.com>
2026-06-09 12:18:11 +0700

    get data API

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,22 +1,10 @@
 import Image from "next/image";
 
-const AnimeList = () => {
+const AnimeList = ({ title, images }) => {
   return (
-    <div className="grid grid-cols-3 gap-2 h-32 ">
-      <div className="bg-indigo-500">
-        <Image
-          src="https://placehold.co/600x400/png"
-          alt="..."
-          width={600}
-          height={600}
-        />
-        <h3>Judul Animenya Disini</h3>
-      </div>
-      <div className="bg-indigo-500">Kotak 2</div>
-      <div className="bg-indigo-500">Kotak 3</div>
-      <div className="bg-indigo-500">Kotak 4</div>
-      <div className="bg-indigo-500">Kotak 5</div>
-      <div className="bg-indigo-500">Kotak 6</div>
+    <div className="bg-indigo-500">
+      <Image src={images} alt="..." width={600} height={600} />
+      <h3>{title}</h3>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 import AnimeList from "@/app/components/AnimeList";
-export default function Home() {
+const Home = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`,
+  );
+
+  const anime = await response.json();
+  console.log("daftar anime", anime);
+
   return (
     <div>
       <h1>Paling Populer</h1>
-      <AnimeList />
+      {anime.data.map((data) => {
+        return (
+          <AnimeList title={data.title} images={data.images.webp.image_url} />
+        );
+      })}
     </div>
   );
-}
+};
+
+export default Home;
