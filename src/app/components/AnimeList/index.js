3fe47a87ea00db56f71ae085736ea3c45f878e64
Roma Debrian <roma_coll04@yahoo.com>
2026-06-10 11:46:05 +0700

    styling card top anime list

--- src/app/components/AnimeList/index.js
+++ src/app/components/AnimeList/index.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const AnimeList = ({ title, images }) => {
+const AnimeList = ({ title, images, id }) => {
   return (
-    <div className="bg-indigo-500">
-      <Image src={images} alt="..." width={600} height={600} />
-      <h3>{title}</h3>
-    </div>
+    <Link href={`/${id}`} className="cursor-pointerf">
+      <Image src={images} alt="..." width={350} height={350} />
+      <h3 className="font-bold md:text-xl text-md p-4 ">{title}</h3>
+    </Link>
   );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -10,11 +10,19 @@ const Home = async () => {
   return (
     <div>
       <h1>Paling Populer</h1>
-      {anime.data.map((data) => {
-        return (
-          <AnimeList title={data.title} images={data.images.webp.image_url} />
-        );
-      })}
+      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+        {anime.data.map((data) => {
+          return (
+            <div key={data.mal_id} className="shadow-xl">
+              <AnimeList
+                title={data.title}
+                images={data.images.webp.image_url}
+                id={data.mal_id}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
